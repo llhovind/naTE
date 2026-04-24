0ae26b12fb70b7b5821c298725a92459b60030bf
Lawrence Hovind <lawrence.hovind@gmail.com>
2026-04-24 16:48:09 +0000

    Fix InputEncoder dropping Tab and Ctrl keys

--- src/session/InputEncoder.cpp
+++ src/session/InputEncoder.cpp
@@ -5,6 +5,13 @@ namespace term::session
 
     std::string InputEncoder::Encode(const ::term::input::KeyEvent &evt)
     {
+        if (evt.ctrl && evt.key == term::input::Key::Character) {
+            const uint32_t lower = (evt.code >= 'A' && evt.code <= 'Z')
+                ? evt.code + 32 : evt.code;
+            if (lower >= 'a' && lower <= 'z')
+                return std::string(1, static_cast<char>(lower - 'a' + 1));
+        }
+
         if (evt.IsPrintable())
         {
             return evt.text;
@@ -16,6 +23,8 @@ namespace term::session
             return "\r";
         case term::input::Key::Backspace:
             return "\x7f";
+        case term::input::Key::Tab:
+            return "\t";
         case term::input::Key::ArrowUp:
             return "\x1b[A";
         case term::input::Key::ArrowDown:
